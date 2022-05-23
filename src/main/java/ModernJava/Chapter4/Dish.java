22b557ecc605c51d24bfc23d917566657ed7c685
yeahyung <yhb77888@naver.com>
2022-05-23 23:27:38 +0900

    Modern Java Chapter ~ 6.3.1

--- src/main/java/ModernJava/Chapter4/Dish.java
+++ src/main/java/ModernJava/Chapter4/Dish.java
@@ -1,5 +1,11 @@
 package ModernJava.Chapter4;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static java.util.Arrays.asList;
+
 public class Dish {
     String name;
     boolean vegetarian;
@@ -30,6 +36,19 @@ public class Dish {
     }
 
     public String toString() {
-        return this.type + " type of " + this.name + " food(" + this.vegetarian + ") has " + this.calories;
+        return this.name;
+    }
+
+    public static final Map<String, List<String>> dishTags = new HashMap<>();
+    static {
+        dishTags.put("pork", asList("greasy", "salty"));
+        dishTags.put("beef", asList("salty", "roasted"));
+        dishTags.put("chicken", asList("fried", "crisp"));
+        dishTags.put("french fries", asList("greasy", "fried"));
+        dishTags.put("rice", asList("light", "natural"));
+        dishTags.put("season fruit", asList("fresh", "natural"));
+        dishTags.put("pizza", asList("tasty", "salty"));
+        dishTags.put("prawns", asList("tasty", "roasted"));
+        dishTags.put("salmon", asList("delicious", "fresh"));
     }
 }
--- src/main/java/ModernJava/Chapter6/CollectDataUsingStream.java
+++ src/main/java/ModernJava/Chapter6/CollectDataUsingStream.java
@@ -1,10 +1,12 @@
 package ModernJava.Chapter6;
 
 import ModernJava.Chapter4.Dish;
+import com.sun.xml.internal.ws.api.model.MEP;
 
 import java.util.*;
 import java.util.stream.Collectors;
 
+import static ModernJava.Chapter4.Dish.dishTags;
 import static java.util.stream.Collectors.*;
 
 public class CollectDataUsingStream {
@@ -44,5 +46,48 @@ public class CollectDataUsingStream {
         IntSummaryStatistics menuStatistics = menu.stream()
                 .collect(summarizingInt(Dish::getCalories));
         System.out.println(menuStatistics);
+
+        // 그룹화
+        Map<String, List<Dish>> dishesByType = menu.stream()
+                .collect(groupingBy(Dish::getType));
+        System.out.println(dishesByType);
+
+        Map<CaloricLevel, List<Dish>> dishesByCaloricLevel = menu.stream()
+                .collect(groupingBy(
+                        dish -> {
+                            if (dish.getCalories() <= 400) {
+                                return CaloricLevel.DIET;
+                            } else if (dish.getCalories() <= 700) {
+                                return CaloricLevel.NORMAL;
+                            } else {
+                                return CaloricLevel.FAT;
+                            }
+                        }
+                ));
+        System.out.println(dishesByCaloricLevel);
+
+        /*
+        이러한 경우, 500 칼로리를 넘는 Fish Type의 메뉴가 없을 경우, Fish 라는 값은 아예 Map의 키에 생성되지 않는다.
+        이러한 문제를 해결하기 위해 Collectors 클래스는 두 번째 매개변수를 인수로 갖도록 오버로드하였다.
+         */
+//        Map<String, List<Dish>> caloricDishesByType = menu.stream()
+//                .filter(dish -> dish.getCalories() > 500)
+//                .collect(groupingBy(Dish::getType));
+//
+        /*
+        java8에서 안되나? 11에서 되는듯
+         */
+//        Map<String, List<Dish>> caloricDishesByType = menu.stream()
+//                .collect(groupingBy(Dish::getType, filtering(dish -> dish.getCalories() > 500, toList()));
+
+        Map<String, List<String>> dishNamesByType = menu.stream()
+                .collect(groupingBy(Dish::getType, mapping(Dish::getName, toList())));
+        System.out.println(dishNamesByType);
+
+
+    }
+
+    public enum CaloricLevel {
+        DIET, NORMAL, FAT
     }
 }
